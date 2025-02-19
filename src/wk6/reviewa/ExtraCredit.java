5e46641f777e4fca0e7e5fcceedef0704a803513
velez <velez@msoe.edu>
2025-02-19 16:25:30 -0600

    wk5 thursday lectures

--- src/wk6/reviewa/ExtraCredit.java
+++ src/wk6/reviewa/ExtraCredit.java
@@ -5,7 +5,14 @@ import java.util.Scanner;
 public class ExtraCredit {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        String date;
+        String word;
         System.out.println("Enter a word.");
+        word = scan.next();
+        for(int i = 0; i<word.length(); i++){
+            if(i % 2 == 0){
+                System.out.println(word.charAt(i));
+            }
+        }
+
     }
 }
--- src/wk6/reviewb/LongQuestion3.java
+++ src/wk6/reviewb/LongQuestion3.java
@@ -9,5 +9,21 @@ public class LongQuestion3 {
         String target;
         System.out.println("Enter a word");
         System.out.println("Enter a target letter");
+
+        word = scan.nextLine();
+        target = scan.nextLine();
+
+        int index = -1;
+
+        for(int i = 0; i<word.length() && index == -1; i++){
+            if(word.charAt(i) == target.charAt(0)){
+                index = i;
+            }
+
+//            if(word.substring(i, i+1).equals(target)){
+//                index = i;
+//            }
+        }
+        System.out.println(index);
     }
 }
